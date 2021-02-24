06a2267ff8f090c146997a4e2e28f12b9b61ac8d
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-02-24 16:07:06 -0300

    Start

---
+++ layout/menu.php
@@ -0,0 +1,94 @@
+<body>
+    <div id="app">
+        <div id="sidebar">
+            <div class="sidebar-wrapper active ps ps--active-y">
+                <div class="m-4">
+                    <a href="index.php">
+                        <img class="img-fluid" src="https://atilasilvalima.a2hosted.com/projects/EGPCE/Cedis/cdn/imagens/egpce.png" alt="Logo EGPCE" srcset="">
+                    </a>
+                </div>
+                <br>
+
+                <div class="sidebar-menu ">
+                    <ul class="menu">
+                        <!--  <li class="sidebar-item">
+                        <a href="../Apresentacao.php" class="sidebar-link">
+                            <i data-feather="home" width="20"></i>
+                            <span>Apresentação</span>
+                        </a>
+                    </li> -->
+                        <li class="sidebar-item">
+                            <a href="Apresentacao.php" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>Apresentação</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item has-sub">
+                            <a href="" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>1 – Gestão por Processos</span>
+                            </a>
+                            <ul class="submenu">
+                                <li class="sidebar-item">
+                                    <a href="Topico01.php" class="sidebar-sub">
+                                        <i data-feather="home" width="20"></i>
+                                        <span>1.1 - O que são e o que não são riscos?</span>
+                                    </a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="Topico01.2.php" class="sidebar-sub">
+                                        <i data-feather="home" width="20"></i>
+                                        <span>1.2 – Quais os riscos que nos interessam?</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="sidebar-item has-sub">
+                            <a href="" class='sidebar-link'>
+                                <i data-feather="home" width="20"></i>
+                                <span>2 – Gestão de Processos</span>
+                            </a>
+                            <ul class="submenu">
+                                <li class="sidebar-item">
+                                    <a href="Topico02.php" class="sidebar-sub">
+                                        <i data-feather="home" width="20"></i>
+                                        <span>2.1 - Gerenciamento para manter processos</span>
+                                    </a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="Topico02.2.php" class="sidebar-sub">
+                                        <i data-feather="home" width="20"></i>
+                                        <span>2.2 - Gerenciamento para melhorar processos</span>
+                                    </a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="Topico02.3.php" class="sidebar-sub">
+                                        <i data-feather="home" width="20"></i>
+                                        <span>2.3 - Como gerenciar riscos?</span>
+                                    </a>
+                                </li>
+                            </ul>
+                            <li class="sidebar-item">
+                                <a href="Topico03.php" class='sidebar-link'>
+                                    <i data-feather="home" width="20"></i>
+                                    <span>3 - O modelo de estrutura integrada de gestão de riscos</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="Topico04.php" class='sidebar-link'>
+                                    <i data-feather="home" width="20"></i>
+                                    <span>4 - Gerenciamento de riscos na Controladoria e Ouvidoria Geral</span>
+                                </a>
+                            </li>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="Referencias.php" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>Referências</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+            </div>
+        </div>
